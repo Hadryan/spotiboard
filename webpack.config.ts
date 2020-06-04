d6acfd71c155a85e9c792a8af05ecc94b3b024ee
Johnny <jl2672@cornell.edu>
2020-06-04 00:38:47 -0700

    historyapifallback true

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
   },
   devtool: 'inline-source-map',
   devServer: {
